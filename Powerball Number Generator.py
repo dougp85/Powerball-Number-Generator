dd42c8878a116c8956adca2152242cd82a493abb
DougPr <54289963+dougp85@users.noreply.github.com>
2020-12-11 15:49:03 -0600

    Add files via upload

---
+++ Powerball Number Generator.py
@@ -0,0 +1,14 @@
+import random
+from random import randrange
+randint = range(1,69)
+
+print('These are the regular numbers: ')
+print(random.randrange(1,69))
+print(random.randrange(1,69))
+print(random.randrange(1,69))
+print(random.randrange(1,69))
+print(random.randrange(1,69))
+
+randint = range(1, 26)
+print('This is the Powerball Number: ' + str(randrange(1,26))'.'
+
